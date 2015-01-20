96473d043b34afc95fa906669750f5a23741cd31
Gean Felipe <geanfelipemoura@outlook.com>
2015-01-20 00:44:46 -0300

    modified media static

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -24,16 +23,18 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-#adc
-TEMPLATE_DIRS  =  [
-# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-# Always use forward slashes, even on Windows.
-# Don't forget to use absolute paths, not relative paths.
-TEMPLATE_PATH,
-]
 
 #adc 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+#adc
+TEMPLATE_DIRS  =  [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+]
+
 ALLOWED_HOSTS = []
 
 
@@ -88,7 +89,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/static/' #ex: http://127.0.0.1:8000/static/ 
 
-STATIC_URL = '/static/'
+#adc
+STATICFILES_DIRS=(
+    STATIC_PATH,
+)
