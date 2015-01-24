feb8c9f5d0a23d64c1b70982a69b7340efb4f03b
Gean Felipe <geanfelipemoura@outlook.com>
2015-01-24 04:52:53 -0300

    [W]***BUG***

--- rango/views.py
+++ rango/views.py
@@ -17,26 +17,30 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def category(request, category_name_slug):
-	context_dict = {}
 
-	try:
-	    # Can we find a category name slug with the given name?
+    # Create a context dictionary which we can pass to the template rendering engine.
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
-		category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name']=category.name
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
 
         # Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
         pages = Page.objects.filter(category=category)
 
         # Adds our results list to the template context under name pages.
-        context_dict['pages']=pages
-
-        # also add the category object from the database to the context dictionary.
-        # ll use this in the template to verify that the category exists.
-        context_dict['category']=category
+        context_dict['pages'] = pages
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
     except Category.DoesNotExist:
-    	pass
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
 
-    return render(request,'rango/category.html',context_dict)
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
