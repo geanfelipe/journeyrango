19cdcee427ac7cb6f29449bd8de25fde99dfc575
Gean Felipe <geanfelipemoura@outlook.com>
2015-01-27 12:19:36 -0300

    modified

--- build/gunicorn/examples/frameworks/django/testing/testing/apps/someapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^acsv$', 'testing.apps.someapp.views.acsv'),
-    url(r'^$', 'testing.apps.someapp.views.home'),
-
-)
