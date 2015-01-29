bb793bf0e434fd30f4ad61ba257407b7beabdd1c
Gean Felipe <geanfelipemoura@outlook.com>
2015-01-29 20:05:15 -0300

    added all

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,6 +3,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_sucess_url(selfself,request,user):
+        return '/rango/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,6 +18,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/',include('rango.urls')), #added this new tuple
+    #(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
 )
 #to upload of files Muitos sites oferecem a seus usuários com a capacidade de fazer isso - por exemplo, para fazer o upload de uma imagem de perfil
